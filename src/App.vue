b027d4eb82b3aab3ccaf62157305e1bc48970037
Naufal <schartregion@gmail.com>
2023-02-13 08:34:18 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <!-- Data & Methods -->
-  <!-- <h2>Nama: {{ nama }}</h2> -->
-  <!-- <button v-on:click="changeName">Change name</button>
-  bisa juga menggunakan @click pada changeName -->
+  <!-- <h2>Nama: {{ nama }}</h2>
+  <button v-on:click="changeName">Change name</button> -->
+  <!-- bisa juga menggunakan @click pada changeName -->
 
   <!-- Data Binding -->
-  <!-- <a v-bind:href="url">Go To Google</a>
-  bisa juga disingkat menjadi :href data binding -->
+  <!-- <a v-bind:href="url">Go To Google</a> -->
+  <!-- bisa juga disingkat menjadi :href data binding -->
 
   <!-- Two-way Data Binding -->
   <!-- <input type="text" v-model="nama" />
@@ -23,46 +23,61 @@
   </div> -->
 
   <!-- Computed -->
-  <!-- <input type="text" v-model="cari" placeholder="cari" />
+  <input type="text" v-model="cari" placeholder="cari" />
   <ul>
     <li v-for="items in cariData" :key="id">{{ items.title }} - {{ items.price }}</li>
-  </ul> -->
+  </ul>
 
   <!-- Componenet & props -->
-  <Header @changeTitle="ubahText" :props="text" />
+  <!-- <Header @changeTitle="ubahText" :props="text" /> -->
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
+// import Header from "@/components/Header.vue";
 export default {
   name: "App",
+  // Data & Methods ---------------------
   // data() {
   //   return {
-  // Data & Methods ---------------------
-  // nama: "Naufal Bambang",
-  // Data Binding ------------------------------
-  // url: "https://www.google.com/",
-  // Two-way Data Binding ---------------------------
-  // nama: "",
-  // For Loops and condition --------------------------
-  // products: [
-  //   { id: 1, title: "Product-1", price: 1000 },
-  //   { id: 2, title: "Product-2", price: 2000 },
-  //   { id: 3, title: "Product-3", price: 3000 },
-  //   { id: 4, title: "Product-4", price: 4000 },
-  //   { id: 5, title: "Product-5", price: 5000 },
-  // ],
-  // Computed
-  // cari: "",
-  // products: [],
+  //     nama: "Naufal Bambang",
   //   };
   // },
   // methods: {
   //   changeName() {
-  //     this.nama = "joko";
+  //     this.nama = "Manuka";
   //   },
   // },
+  // Data Binding ------------------------------
+  // data() {
+  //   return {
+  //     url: "https://www.google.com/",
+  //   };
+  // },
+  // Two-way Data Binding ---------------------------
+  // data() {
+  //   return {
+  //     nama: "",
+  //   };
+  // },
+  // For Loops and condition --------------------------
+  // data() {
+  //   return {
+  //     products: [
+  //       { id: 1, title: "Product-1", price: 1000 },
+  //       { id: 2, title: "Product-2", price: 2000 },
+  //       { id: 3, title: "Product-3", price: 3000 },
+  //       { id: 4, title: "Product-4", price: 4000 },
+  //       { id: 5, title: "Product-5", price: 5000 },
+  //     ],
+  //   };
+  // },
 
+  // data() {
+  //   return {
+  //     cari: "",
+  //     products: [],
+  //   };
+  // },
   // Lifecycle hook
   // created() {
   //   this.products = [
@@ -73,7 +88,6 @@ export default {
   //     { id: 5, title: "Product-5", price: 5000 },
   //   ];
   // },
-  // Computed
   // computed: {
   //   cariData() {
   //     return this.products.filter((item) => {
@@ -82,19 +96,19 @@ export default {
   //   },
   // },
 
-  components: {
-    Header,
-  },
-  data() {
-    return {
-      text: "Halo aku data dari app",
-    };
-  },
-  methods: {
-    ubahText(newText) {
-      this.text = newText;
-    },
-  },
+  // components: {
+  //   Header,
+  // },
+  // data() {
+  //   return {
+  //     text: "Halo aku data dari app",
+  //   };
+  // },
+  // methods: {
+  //   ubahText(newText) {
+  //     this.text = newText;
+  //   },
+  // },
 };
 </script>
 
@@ -106,5 +120,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+a {
+  text-decoration: none;
+  color: red;
+  font-weight: 700;
 }
 </style>
